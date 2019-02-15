5646145b73b184568de6ff287c3ca797f496d7a6
Paul Fulham <paul.fulham0@gmail.com>
2019-02-15 10:19:08 -0800

    Restructure PaletteFactory

--- app/src/main/java/io/github/formular_team/formular/color/PaletteFactory.java
+++ app/src/main/java/io/github/formular_team/formular/color/PaletteFactory.java
@@ -10,18 +10,38 @@ public interface PaletteFactory {
     interface Builder {
         Builder size(final Range<Integer> size);
 
-        Builder red(final Range<Float> red);
-
-        Builder green(final Range<Float> green);
-
-        Builder blue(final Range<Float> blue);
-
-        Builder hue(final Range<Float> hue);
-
-        Builder saturation(final Range<Float> saturation);
-
-        Builder value(final Range<Float> value);
+        Builder color(final ColorRange color);
 
         PaletteFactory build();
+    }
+
+    interface ColorRange {
+        Range<Float> red();
+
+        Range<Float> green();
+
+        Range<Float> blue();
+
+        Range<Float> hue();
+
+        Range<Float> saturation();
+
+        Range<Float> value();
+
+        interface Builder {
+            Builder red(final Range<Float> red);
+
+            Builder green(final Range<Float> green);
+
+            Builder blue(final Range<Float> blue);
+
+            Builder hue(final Range<Float> hue);
+
+            Builder saturation(final Range<Float> saturation);
+
+            Builder value(final Range<Float> value);
+
+            ColorRange build();
+        }
     }
 }
