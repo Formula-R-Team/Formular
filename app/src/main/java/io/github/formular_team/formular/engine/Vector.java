64bdb1d53ea47463df69d131df988d35c02c3133
Paul Fulham <paul.fulham0@gmail.com>
2019-02-15 10:58:57 -0800

    Add Vector3

--- app/src/main/java/io/github/formular_team/formular/engine/Vector.java
+++
@@ -1,7 +0,0 @@
-package io.github.formular_team.formular.engine;
-
-public interface Vector {
-    float x();
-    float y();
-    float z();
-}
---
+++ app/src/main/java/io/github/formular_team/formular/math/Vector3.java
@@ -0,0 +1,7 @@
+package io.github.formular_team.formular.math;
+
+public interface Vector3 {
+    float x();
+    float y();
+    float z();
+}
