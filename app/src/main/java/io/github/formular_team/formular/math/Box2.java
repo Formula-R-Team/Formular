5dd4711aa5681f8a82b97ab5e35d611d797e987d
Paul Fulham <paul.fulham0@gmail.com>
2019-02-19 12:13:23 -0800

    Add stub shape classes

---
+++ app/src/main/java/io/github/formular_team/formular/math/Box2.java
@@ -0,0 +1,78 @@
+package io.github.formular_team.formular.math;
+
+// https://threejs.org/docs/#api/en/math/Box2
+public final class Box2 {
+    public Box2(final Vector2 min, final Vector2 max) {
+        // TODO
+    }
+
+    public Vector2 min() {
+        // TODO
+        return null;
+    }
+
+    public Vector2 max() {
+        // TODO
+        return null;
+    }
+
+    public Vector2 clampPoint(final Vector2 point, final Vector2 target) {
+        // TODO
+        return null;
+    }
+
+    public Box2 copy() {
+        // TODO
+        return null;
+    }
+
+    public boolean containsBox(final Box2 box) {
+        // TODO
+        return false;
+    }
+
+    public boolean containsPoint(final Vector2 point) {
+        // TODO
+        return false;
+    }
+
+    public Box2 expandByPoint(final Vector2 point) {
+        // TODO
+        return this;
+    }
+
+    public Vector2 getCenter(final Vector2 target) {
+        // TODO
+        return null;
+    }
+
+    public Vector2 getSize(final Vector2 target) {
+        // TODO
+        return null;
+    }
+
+    public boolean isEmpty() {
+        // TODO
+        return false;
+    }
+
+    public Box2 set(final Vector2 min, final Vector2 max) {
+        // TODO
+        return this;
+    }
+
+    public Box2 setFromCenterAndSize(final Vector2 center, final Vector2 size) {
+        // TODO
+        return this;
+    }
+
+    public Box2 setFromPoints(final Vector2[] points) {
+        // TODO
+        return this;
+    }
+
+    public Box2 translate(final Vector2 offset) {
+        // TODO
+        return this;
+    }
+}
