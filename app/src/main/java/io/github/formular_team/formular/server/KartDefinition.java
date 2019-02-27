047f9d2862b7518dc93f69967c0f91c3bb92d81a
Gabe Eatmon <g.eatmon@comcast.net>
2019-02-26 18:32:06 -0800

    Turned KartDefinition from an interface to a class
    Implemented the KartDefinition from an interface into a concrete class.

--- app/src/main/java/io/github/formular_team/formular/server/KartDefinition.java
+++ app/src/main/java/io/github/formular_team/formular/server/KartDefinition.java
@@ -1,21 +1,70 @@
 package io.github.formular_team.formular.server;
 
-public interface KartDefinition {
-    String name();
+import com.google.common.collect.Range;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static io.github.formular_team.formular.util.MorePreconditions.checkBounded;
 
-    float size();
+public final class KartDefinition {
+    private final String name;
 
-    float mass();
+    private final float size;
 
-    float acceleration();
+    private final float mass;
 
-    interface Builder {
-        Builder name(final String name);
+    private final float acceleration;
 
-        Builder size(final float size);
+    private KartDefinition(final Builder builder){
+        this.name = builder.name;
+        this.size = builder.size;
+        this.mass = builder.mass;
+        this.acceleration = builder.acceleration;
+    }
 
-        Builder mass(final float mass);
+    public String getName(){return this.name;}
 
-        KartDefinition build();
+    public float getSize(){return this.size;}
+
+    public float getMass(){return this.mass;}
+
+    public float getAcceleration(){return this.acceleration;}
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public final static class Builder {
+
+        private String name;
+        private float size;
+        private float mass;
+        private float acceleration;
+
+        private Builder(){}
+
+        public Builder name(final String name){
+            checkNotNull(name);
+            this.name = name;
+            return this;
+        }
+
+        public Builder size(final float size){
+            checkNotNull(size);
+            this.size = size;
+            return this;
+        }
+
+        public Builder mass(final float mass){
+            checkNotNull(mass);
+            this.mass = mass;
+            return this;
+        }
+
+        public Builder acceleration(final float acceleration){
+            checkNotNull(acceleration);
+            this.acceleration = acceleration;
+            return this;
+        }
+
+        public KartDefinition build(){return new KartDefinition(this);}
     }
 }
