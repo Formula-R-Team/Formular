da4f0af10587a748208ccf0509533dffc9e2ac69
Gabe Eatmon <g.eatmon@comcast.net>
2019-03-02 15:01:05 -0800

    Created SceneryItem Class

--- app/src/main/java/io/github/formular_team/formular/server/SceneryItem.java
+++ app/src/main/java/io/github/formular_team/formular/server/SceneryItem.java
@@ -2,8 +2,30 @@ package io.github.formular_team.formular.server;
 
 import io.github.formular_team.formular.math.Matrix3;
 
-public interface SceneryItem {
-    NamespacedString type();
+public final class SceneryItem {
 
-    Matrix3 transform();
+    private NamespacedString mType;
+
+    private Matrix3 mTransform;
+
+    SceneryItem(NamespacedString type, Matrix3 transform){
+        mType = type;
+        mTransform = transform;
+    }
+
+    public NamespacedString getType(){
+        return mType;
+    }
+
+    public Matrix3 getTransform(){
+        return mTransform;
+    }
+
+    public void setType(NamespacedString type){
+        mType = type;
+    }
+
+    public void setTransform(Matrix3 transform){
+        mTransform = transform;
+    }
 }
