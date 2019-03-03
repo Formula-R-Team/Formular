22b117a57b466103ca4b65d5cdb1bb1405f2f283
Stan Kozubenko <staspk@gmail.com>
2019-03-03 14:00:44 -0800

    Update Track.java

--- app/src/main/java/io/github/formular_team/formular/server/Track.java
+++ app/src/main/java/io/github/formular_team/formular/server/Track.java
@@ -6,9 +6,7 @@ import io.github.formular_team.formular.math.Path;
 import io.github.formular_team.formular.math.Shape;
 import io.github.formular_team.formular.math.Vector2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class Track {
 
@@ -38,38 +36,37 @@ public class Track {
     public ImmutableList<CheckPoint> getCheckPoints(){return this.checkPoints;}
 
 
-    public static TrackBuilder builder(Path roadPath, Shape roadShape) {
-        return new TrackBuilder(roadPath, roadShape);
+    public static TrackBuilder builder() {
+        return new TrackBuilder();
     }
 
-    public static class TrackBuilder {
+    public static final class TrackBuilder {
         private Path roadPath;
         private Shape roadShape;
         private ImmutableList<CheckPoint> checkPoints;
 
-        TrackBuilder(Path roadPath, Shape roadShape) {
+        private TrackBuilder() {}
+
+        public TrackBuilder roadPath(final Path roadPath) {
+            checkNotNull(this.roadPath, "Road Path Required for TrackBuilder");
             this.roadPath = roadPath;
+            return this;
+        }
+
+        public TrackBuilder roadShape(final Shape roadShape) {
+            checkNotNull(this.roadShape, "Road Shape Required for TrackBuilder");
             this.roadShape = roadShape;
-            this.checkPoints = determineCheckpoints();
+            return this;
+        }
+
+        public TrackBuilder checkPoints(final ImmutableList<CheckPoint> checkPoints) {
+            checkNotNull(this.checkPoints, "Checkpoints are Required for TrackBuilder");
+            this.checkPoints = checkPoints;
+            return this;
         }
 
         public Track build() {
             return new Track(this);
-        }
-
-        //Method definitely needs to be changed to spit out real checkpoints
-        private ImmutableList<CheckPoint> determineCheckpoints(){
-
-            ArrayList<CheckPoint> list = new ArrayList<CheckPoint>();
-
-            Vector2 point1 = this.roadPath.getPoint(1f);
-            Vector2 point2 = this.roadPath.getPoint(1f);
-            Vector2 point3 = this.roadPath.getPoint(2f);
-            Vector2 point4 = this.roadPath.getPoint(2f);
-
-            list.addAll(Arrays.asList(new CheckPoint(point1, point2), new CheckPoint(point3, point4)));
-
-            return ImmutableList.copyOf(list);
         }
     }
 }
