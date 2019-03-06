1646222185621b7937303545f0453d5497ee2eaa
Paul Fulham <paul.fulham0@gmail.com>
2019-03-05 19:45:49 -0800

    Add transform path visitor

--- app/src/main/java/io/github/formular_team/formular/math/PathVisitor.java
+++ app/src/main/java/io/github/formular_team/formular/math/PathVisitor.java
@@ -1,5 +1,7 @@
 package io.github.formular_team.formular.math;
 
+import java.util.function.Function;
+
 public interface PathVisitor {
     PathVisitor moveTo(final Vector2 point);
 
@@ -8,4 +10,32 @@ public interface PathVisitor {
     PathVisitor bezierCurveTo(final Vector2 controlA, final Vector2 controlB, final Vector2 point);
 
     PathVisitor closePath();
+
+    default PathVisitor transform(final Function<Vector2, Vector2> function) {
+        return new PathVisitor() {
+            @Override
+            public PathVisitor moveTo(final Vector2 point) {
+                return PathVisitor.this.moveTo(this.transform(point));
+            }
+
+            @Override
+            public PathVisitor lineTo(final Vector2 point) {
+                return PathVisitor.this.lineTo(this.transform(point));
+            }
+
+            @Override
+            public PathVisitor bezierCurveTo(final Vector2 controlA, final Vector2 controlB, final Vector2 point) {
+                return PathVisitor.this.bezierCurveTo(this.transform(controlA), this.transform(controlB), this.transform(point));
+            }
+
+            @Override
+            public PathVisitor closePath() {
+                return PathVisitor.this.closePath();
+            }
+
+            private Vector2 transform(final Vector2 vector) {
+                return function.apply(vector);
+            }
+        };
+    }
 }
