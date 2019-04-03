1995138e2d7ff08ca1237df7ffa1c860507f466c
Paul Fulham <paul.fulham0@gmail.com>
2019-04-03 10:51:41 -0700

    Continue rectifier

--- app/src/main/java/io/github/formular_team/formular/Rectifier.java
+++ app/src/main/java/io/github/formular_team/formular/Rectifier.java
@@ -19,6 +19,8 @@ public class Rectifier implements Closeable  {
 
     private final Image capture;
 
+    private final Rect wholeCapture;
+
     private final float[] viewProjMat = new float[16];
 
     private final float[] mvp = new float[16];
@@ -26,6 +28,7 @@ public class Rectifier implements Closeable  {
     public Rectifier(final Frame frame) throws NotYetAvailableException {
         this.frame = frame;
         this.capture = frame.acquireCameraImage();
+        this.wholeCapture = new Rect(0, 0, this.capture.getWidth(), this.capture.getHeight());
         final Camera camera = frame.getCamera();
         final float[] projMat = new float[16];
         camera.getProjectionMatrix(projMat, 0, 0.1F, 10.0F);
@@ -38,43 +41,93 @@ public class Rectifier implements Closeable  {
         final float[] modelMat = new float[16];
         model.toMatrix(modelMat, 0);
         Matrix.multiplyMM(this.mvp, 0, this.viewProjMat, 0, modelMat, 0);
-        final float[] world = {
-            -range, 0.0F,  range, 1.0F,
-            -range, 0.0F, -range, 1.0F,
-             range, 0.0F,  range, 1.0F,
-             range, 0.0F, -range, 1.0F
-        };
-        final float[] pixel = new float[2 * 4];
-        this.transform(world, pixel);
-        final RectF rect = new RectF(Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY, Float.NEGATIVE_INFINITY, Float.NEGATIVE_INFINITY);
+        final int count = resolution * resolution;
+        final int buflen = Math.max(4, count);
+        final float[] in = new float[4 * buflen];
+        final float[] ndc = new float[2 * buflen];
+        final float[] out = new float[2 * buflen];
+        setVec4(in, 0, -range, 0.0F, range, 1.0F);
+        setVec4(in, 1, -range, 0.0F, -range, 1.0F);
+        setVec4(in, 2, range, 0.0F, range, 1.0F);
+        setVec4(in, 3, range, 0.0F, -range, 1.0F);
+        this.transform(in, ndc, out, 4);
+        final RectF captureBounds = new RectF(
+            Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY,
+            Float.NEGATIVE_INFINITY, Float.NEGATIVE_INFINITY
+        );
         for (int i = 1; i < 4; i++) {
-            rect.union(pixel[0 + 2 * i], pixel[1 + 2 * i]);
+            captureBounds.union(getVec2X(out, i), getVec2Y(out, i));
         }
-        final Rect r = new Rect();
-        rect.roundOut(r);
-        r.intersect(new Rect(0, 0, this.capture.getWidth() - 1, this.capture.getHeight() - 1));
-        final Bitmap image = Images.yuvToBitmap(this.capture, r);
-        return null;
+        final Rect captureRegion = new Rect();
+        captureBounds.roundOut(captureRegion);
+        if (!captureRegion.intersect(this.wholeCapture)) {
+            return null;
+        }
+        final Bitmap subcapture = Images.yuvToBitmap(this.capture, captureRegion);
+        if (subcapture == null) {
+            return null;
+        }
+        final Bitmap rectified = Bitmap.createBitmap(resolution, resolution, Bitmap.Config.ARGB_8888);
+        for (int y = 0; y < resolution; y++) {
+            for (int x = 0; x < resolution; x++) {
+                setVec4(in,
+                    x + y * resolution,
+                    range * (2.0F * x / resolution - 1.0F),
+                    0.0F,
+                    -range * (2.0F * y / resolution - 1.0F),
+                    1.0F
+                );
+            }
+        }
+        this.transform(in, ndc, out, count);
+        for (int y = 0; y < resolution; y++) {
+            for (int x = 0; x < resolution; x++) {
+                final int i = x + y * resolution;
+                final int captureX = (int) (getVec2X(out, i) - captureRegion.left);
+                final int captureY = (int) (getVec2Y(out, i) - captureRegion.top);
+                if (captureRegion.contains(captureX, captureY)) {
+                    rectified.setPixel(x, y, subcapture.getPixel(captureX, captureY));
+                }
+            }
+        }
+        return rectified;
     }
 
-    private void transform(final float[] worldIn, final float[] pixelOut) {
-        final int count = worldIn.length / 4;
-        final float[] ndcIn = new float[2 * count];
+    private void transform(final float[] modelspace, final float[] ndc, final float[] pixelspace, final int count) {
         final float[] vec = new float[4];
         for (int i = 0; i < count; i++) {
-            Matrix.multiplyMV(vec, 0, this.mvp, 0, worldIn, 4 * i);
+            Matrix.multiplyMV(vec, 0, this.mvp, 0, modelspace, 4 * i);
             final float d = 1.0F / vec[3];
-            ndcIn[0 + 2 * i] = vec[0] * d;
-            ndcIn[1 + 2 * i] = vec[1] * d;
+            setVec2(ndc, i, vec[0] * d, vec[1] * d);
         }
         this.frame.transformCoordinates2d(
-            Coordinates2d.OPENGL_NORMALIZED_DEVICE_COORDINATES, ndcIn,
-            Coordinates2d.IMAGE_PIXELS, pixelOut
+            Coordinates2d.OPENGL_NORMALIZED_DEVICE_COORDINATES, ndc,
+            Coordinates2d.IMAGE_PIXELS, pixelspace
         );
     }
 
     @Override
     public void close() {
         this.capture.close();
+    }
+
+    private static void setVec4(final float[] buf, final int index, final float x, final float y, final float z, final float w) {
+        buf[0 + 4 * index] = x;
+        buf[1 + 4 * index] = y;
+        buf[2 + 4 * index] = z;
+        buf[3 + 4 * index] = w;
+    }
+
+    private static void setVec2(final float[] buf, final int index, final float x, final float y) {
+        buf[0 + 2 * index] = x;
+        buf[1 + 2 * index] = y;
+    }
+
+    private static float getVec2X(final float[] buf, final int index) {
+        return buf[0 + 2 * index];
+    }
+
+    private static float getVec2Y(final float[] buf, final int index) {
+        return buf[1 + 2 * index];
     }
 }
