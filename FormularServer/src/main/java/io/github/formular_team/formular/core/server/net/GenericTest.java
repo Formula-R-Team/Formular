ce8bd2bcdf2cf94dfabe4e7adeb547c1e06ce2ce
Paul Fulham <paul.fulham0@gmail.com>
2019-05-01 09:48:58 -0700

    Continue generic test

--- FormularServer/src/main/java/io/github/formular_team/formular/core/server/net/GenericTest.java
+++ FormularServer/src/main/java/io/github/formular_team/formular/core/server/net/GenericTest.java
@@ -5,22 +5,34 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public class GenericTest {
-    static class Builder {
-
+    static class ConsumerTree<K, V> {
+        Consumer<V> get(final K key) {
+            return null;
+        }
     }
 
-    static abstract class Node {}
+    static abstract class Node<K, V> {
+        abstract Consumer<? super V> get(final K key);
+    }
 
-    static class MapNode<K, V> {
-        final MapNode<K, ? super V> parent;
+    static class Root<K, V> extends Node<K, V> {
+        @Override
+        Consumer<? super V> get(final K key) {
+            return t -> {};
+        }
+    }
+
+    static class MapNode<K, V> extends Node<K, V> {
+        final Node<K, ? super V> parent;
 
         final Map<K, Consumer<V>> map;
 
-        MapNode(final MapNode<K, ? super V> parent, final Map<K, Consumer<V>> map) {
+        MapNode(final Node<K, ? super V> parent, final Map<K, Consumer<V>> map) {
             this.parent = parent;
             this.map = map;
         }
 
+        @Override
         Consumer<? super V> get(final K key) {
             final Consumer<V> v = this.map.get(key);
             return v == null ? this.parent.get(key) : v;
@@ -42,19 +54,10 @@ public class GenericTest {
     }
 
     public static void builder() {
-        /*final Node n = new Builder(Context.class)
-            .put("root", (Context context) -> {})
-            .add(new Builder(FooContext.class)
-                .put("foo", (FooContext context) -> {})
-            )
-            .build();*/
     }
 
     public static void node() {
-        final MapNode<String, Context> root = new MapNode<>(null, new HashMap<>());
-        root.map.put("root", (Context context) -> {});
-        root.get("root").accept(new Context());
-        root.get("root").accept(new FooContext());
+        final Node<String, Context> root = new Root<>();
         final MapNode<String, FooContext> foo = new MapNode<>(root, new HashMap<>());
         foo.map.put("foo", (FooContext context) -> {});
         foo.get("foo").accept(new FooContext());
