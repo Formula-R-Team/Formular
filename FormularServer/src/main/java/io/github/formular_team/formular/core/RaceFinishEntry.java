27b5d2a9e6b21e67206162fac914b56b36c85c77
Paul Fulham <paul.fulham0@gmail.com>
2019-05-05 12:07:12 -0700

    Update race finish entry

--- FormularServer/src/main/java/io/github/formular_team/formular/core/RaceFinishEntry.java
+++ FormularServer/src/main/java/io/github/formular_team/formular/core/RaceFinishEntry.java
@@ -3,22 +3,15 @@ package io.github.formular_team.formular.core;
 public final class RaceFinishEntry {
     private final User user;
 
-    private final int position;
-
     private final long time;
 
-    public RaceFinishEntry(final User user, final int position, final long time) {
+    public RaceFinishEntry(final User user, final long time) {
         this.user = user;
-        this.position = position;
         this.time = time;
     }
 
     public User getUser() {
         return this.user;
-    }
-
-    public int getPosition() {
-        return this.position;
     }
 
     public long getTime() {
