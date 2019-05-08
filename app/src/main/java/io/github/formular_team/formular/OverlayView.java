7044e45c02219db84d83eb968faf5af15137a977
Joseph Carlson <joseph.carlson@eagles.ewu.edu>
2019-05-08 13:14:04 -0700

    Added OverlayView preference and made SandboxActivity implement it

---
+++ app/src/main/java/io/github/formular_team/formular/OverlayView.java
@@ -0,0 +1,15 @@
+package io.github.formular_team.formular;
+
+import android.support.annotation.StringRes;
+
+public interface OverlayView
+{
+
+    void setCount(@StringRes int resID);
+
+    void setPosition(@StringRes int resID);
+
+    void setLap(@StringRes int resID);
+
+
+}
