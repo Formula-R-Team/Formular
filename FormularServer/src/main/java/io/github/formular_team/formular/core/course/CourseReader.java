a8ad580e42efa1789e7fa58ac052b2c53cae43bb
Paul Fulham <paul.fulham0@gmail.com>
2019-05-22 13:40:54 -0700

    Start course canonicalization

--- FormularServer/src/main/java/io/github/formular_team/formular/core/course/CourseReader.java
+++ FormularServer/src/main/java/io/github/formular_team/formular/core/course/CourseReader.java
@@ -1,14 +1,18 @@
 package io.github.formular_team.formular.core.course;
 
+import java.util.List;
+
 import io.github.formular_team.formular.core.BezierPathSimplifier;
 import io.github.formular_team.formular.core.SimpleTrackFactory;
 import io.github.formular_team.formular.core.course.track.Track;
 import io.github.formular_team.formular.core.math.Matrix3;
 import io.github.formular_team.formular.core.math.Mth;
-import io.github.formular_team.formular.core.math.curve.Path;
 import io.github.formular_team.formular.core.math.TransformingPathVisitor;
+import io.github.formular_team.formular.core.math.Vector2;
+import io.github.formular_team.formular.core.math.curve.Path;
 import io.github.formular_team.formular.core.tracing.CirclePathLocator;
 import io.github.formular_team.formular.core.tracing.OrientFunction;
+import io.github.formular_team.formular.core.tracing.PCA;
 import io.github.formular_team.formular.core.tracing.PathFinder;
 import io.github.formular_team.formular.core.tracing.SimplePathTracer;
 import io.github.formular_team.formular.core.tracing.SimpleStepFunction;
@@ -16,8 +20,7 @@ import io.github.formular_team.formular.core.tracing.SimpleStepFunction;
 // TODO: good course creation api
 public class CourseReader {
     public void read(final Capture capture, final CourseMetadata metadata, final ResultConsumer consumer) {
-        final float courseRoadWidth = 5.0F;
-        final float courseToSceneScale = 0.06F / courseRoadWidth;
+        final float courseToSceneScale = 0.012F;
         final float courseCaptureSize = capture.getRadius() / courseToSceneScale;
         new PathFinder(
             new CirclePathLocator(25),
@@ -28,14 +31,20 @@ public class CourseReader {
             .find(capture.getImage(), new PathFinder.ResultConsumer() {
                 @Override
                 public void onClosed(final Path path) {
-                    final Path linePath = new Path();
-                    path.visit(new TransformingPathVisitor(linePath, new Matrix3()
-                        .scale(2.0F / capture.getResolution())
-                        .translate(-1.0F, -1.0F)
-                        .scale(courseCaptureSize, -courseCaptureSize)
+                    final Path worldPath = new Path();
+                    path.visit(new TransformingPathVisitor(worldPath, new Matrix3()
+                        .multiply(new Matrix3().scale(2.0F / capture.getResolution()))
+                        .multiply(new Matrix3().translate(-1.0F, -1.0F))
+                        .multiply(new Matrix3().scale(courseCaptureSize, -courseCaptureSize))
                     ));
-                    final Path simplifiedPath = BezierPathSimplifier.create(0.175F).simplify(linePath);
-                    final Track track = new SimpleTrackFactory(courseRoadWidth).create(simplifiedPath);
+                    final Path normalPath = new Path();
+                    final TrackPose pose = get(worldPath);
+                    worldPath.visit(new TransformingPathVisitor(normalPath, new Matrix3()
+                        .multiply(new Matrix3().translate(-pose.position.getX(), -pose.position.getY()))
+                        .multiply(new Matrix3().rotate(-pose.ellipse.getAngle()))
+                    ));
+                    final Path simplifiedPath = BezierPathSimplifier.create(0.175F).simplify(normalPath);
+                    final Track track = new SimpleTrackFactory(5.0F).create(simplifiedPath);
                     if (track.getCheckpoints().size() >= 5) {
                         final Course course = Course.builder()
                             .setMetadata(metadata)
@@ -58,6 +67,33 @@ public class CourseReader {
                     consumer.onFail();
                 }
             });
+    }
+
+    static class TrackPose {
+        private final Vector2 position;
+
+        private final PCA.Ellipse ellipse;
+
+        TrackPose(final Vector2 position, final PCA.Ellipse ellipse) {
+            this.position = position;
+            this.ellipse = ellipse;
+        }
+    }
+
+    public static TrackPose get(final Path path) {
+        final List<Vector2> points = path.getPoints(false);
+        final int n = points.size();
+        final float[] x = new float[n], y = new float[n], w = new float[n];
+        final Vector2 avg = new Vector2();
+        for (int i = 0; i < n ; i++) {
+            final Vector2 point = points.get(i);
+            x[i] = point.getX();
+            y[i] = point.getY();
+            w[i] = 1.0F;
+            avg.add(point);
+        }
+        avg.divide(n);
+        return new TrackPose(avg, PCA.get(x, y, w, n));
     }
 
     public interface ResultConsumer {
